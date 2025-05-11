207268810151f09fcb2aa236899536310fe635e3
Kaedo17 <92620393+Kaedo17@users.noreply.github.com>
2025-05-11 22:14:05 +0800

    start

--- database.h
+++ database.h
@@ -1,96 +1,29 @@
 #ifndef DATABASE
 #define DATABASE
-
-#include <vector>
-#include <string>
-#include <tuple>
-#include <fstream>
 #include <iostream>
-#include <stdexcept>
+#include <fstream>
+#include <vector>
 
-class ToDoDataBase {
-private:
-    std::vector<std::tuple<std::string, bool>> toDoList;
-    std::string filename = "tododatabase.dat";
+class toDoDataBase {
+    vector<string> toDoList = {};
 
-    // Helper function to serialize data to file
-    void saveToFile() {
-        std::ofstream outFile(filename, std::ios::binary);
-        if (!outFile) {
-            throw std::runtime_error("Could not open file for writing");
-        }
-
-        size_t size = toDoList.size();
-        outFile.write(reinterpret_cast<const char*>(&size), sizeof(size_t));
-        
-        for (const auto& item : toDoList) {
-            const std::string& task = std::get<0>(item);
-            bool completed = std::get<1>(item);
-            
-            size_t taskSize = task.size();
-            outFile.write(reinterpret_cast<const char*>(&taskSize), sizeof(size_t));
-            outFile.write(task.c_str(), taskSize);
-            outFile.write(reinterpret_cast<const char*>(&completed), sizeof(bool));
-        }
-    }
-
-    // Helper function to deserialize data from file
-    void loadFromFile() {
-        std::ifstream inFile(filename, std::ios::binary);
-        if (!inFile) {
-            // File doesn't exist yet, use initial data
-            createInitialData();
-            return;
-        }
-
-        size_t size;
-        inFile.read(reinterpret_cast<char*>(&size), sizeof(size_t));
-        
-        toDoList.clear();
-        toDoList.reserve(size);
-        
-        for (size_t i = 0; i < size; ++i) {
-            size_t taskSize;
-            inFile.read(reinterpret_cast<char*>(&taskSize), sizeof(size_t));
-            
-            std::string task(taskSize, '\0');
-            inFile.read(&task[0], taskSize);
-            
-            bool completed;
-            inFile.read(reinterpret_cast<char*>(&completed), sizeof(bool));
-            
-            toDoList.emplace_back(task, completed);
-        }
-    }
-
-public:
-    // First time open the app
     void createInitialData() {
         toDoList = {
-            {"Cook an egg!", false}
-        };
-        saveToFile();
+            {"Cook me", false}
+    };
     }
 
-    // Load data from the database
+
+int sizeOfToDo = sizeof(toDoList)/sizeof(toDoList[0]);
     void loadData() {
-        loadFromFile();
+        for (int i = 0; i < sizeOfToDo; i++) {
+            cout << toDoList[i];
+        }
     }
 
-    // Update database
     void updateDataBase() {
-        saveToFile();
-    }
-
-    // Getter for the to-do list
-    const std::vector<std::tuple<std::string, bool>>& getToDoList() const {
-        return toDoList;
-    }
-
-    // Setter for the to-do list
-    void setToDoList(const std::vector<std::tuple<std::string, bool>>& newList) {
-        toDoList = newList;
+        toDoList.push_back
     }
 };
 
-#endif 
+#endif
